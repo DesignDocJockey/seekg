12f71bdbe3c2845233645d0e68cdc91b2d410567
DesignDocJockey <hwong18@gmail.com>
2018-12-17 23:50:19 -0500

    started some shit

---
+++ src/ManifestParser.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+using System.Linq;
+
+namespace SectionNormalization.src
+{
+    public static class ManifestParser
+    {
+        public static IEnumerable<RawManifestRecord> ParseManifestFile(string manifestFile)
+        {
+            if (!File.Exists(manifestFile))
+                throw new FileNotFoundException($"File: {manifestFile} was not found.");
+
+            var rawFileContents = File.ReadAllLines(manifestFile)
+                                        .ToList()
+                                        .Select(i => ParseRawManifestRecord(i));
+            return rawFileContents;
+        }
+
+        private static RawManifestRecord ParseRawManifestRecord(string rawRecord) {
+            var lineItem = rawRecord.Split(',');
+            //TODO::continue here
+        }
+    }
+}
---
+++ src/RawManifestRecord.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SectionNormalization.src
+{
+    public class RawManifestRecord
+    {
+        public string Section { get; set; }
+        public string Row { get; set; }
+        public int SectionId { get; set; }
+        public int RowId { get; set}
+        public bool Valid { get; set; }
+    }
+}
